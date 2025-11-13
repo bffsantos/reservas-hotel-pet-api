ca64d943701f6c4c4396bb2c8fdbe404b31f31a7
bffsantos <brunoffsantos_2010@hotmail.com>
2025-11-13 03:48:30 -0300

    Update AnimalDTO

--- ReservasHotelPetAPI/DTOs/AnimalDTO.cs
+++ ReservasHotelPetAPI/DTOs/AnimalDTO.cs
@@ -6,17 +6,12 @@ namespace ReservasHotelPetAPI.DTOs
     public class AnimalDTO
     {
         public int Id { get; set; }
-        [Required]
         [StringLength(80)]
         public string? Nome { get; set; }
-        [Required]
         public int Idade { get; set; }
-        [Required]
         public char Sexo { get; set; }
-        [Required]
         [StringLength(50)]
         public string? Tipo { get; set; }
-        [Required]
         [StringLength(50)]
         public string? Raca { get; set; }
         [Column("TutorId")]
